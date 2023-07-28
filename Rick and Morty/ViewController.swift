fda3609f5b44be64fbd2ae4a8abbe852607235f2
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2023-07-28 18:49:20 +0300

    Initial Commit

---
+++ Rick and Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty
+//
+//  Created by Mehmet Talha Irmak on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
