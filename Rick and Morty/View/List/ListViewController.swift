f5c1f3c035842e3b2e68c792cb9c492ceb8bbb30
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2023-07-30 18:11:46 +0300

    Integrated ListViewController to viewModel and maked screen body

--- Rick and Morty/View/List/ListViewController.swift
+++ Rick and Morty/View/List/ListViewController.swift
@@ -10,12 +10,50 @@ import SnapKit
 
 final class ListViewController: UIViewController {
     private let labelTitle: UILabel = UILabel()
-    private let box: UIView = UIView()
+    private let tableView: UITableView = UITableView()
     private let indicator: UIActivityIndicatorView = UIActivityIndicatorView()
-     
+    
+    private lazy var viewModel: ListViewModelInterface = ListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel.view = self
+        viewModel.viewDidLoad()
+        
         configureUI()
+    }
+}
+
+//MARK: - TableView Delegate & DataSource
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.rickyMortyCharacters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: RickyMortyCell = tableView.dequeueReusableCell(withIdentifier: RickyMortyCell.Identifier.custom.rawValue) as? RickyMortyCell else {
+            return UITableViewCell()
+        }
+        
+        cell.saveModel(result: viewModel.rickyMortyCharacters[indexPath.row])
+        
+        return cell
+    }
+}
+
+//MARK: - ListViewModel Method Transferring
+extension ListViewController: ListViewInterface {
+    func prepareListView() {
+        
+    }
+    
+    func changeLoading(isLoad: Bool) {
+        isLoad ? indicator.startAnimating() : indicator.stopAnimating()
+    }
+    
+    func saveDatas(values: [Result]) {
+        tableView.reloadData()
     }
 }
 
@@ -25,25 +63,30 @@ extension ListViewController {
         view.backgroundColor = .white
         
         view.addSubview(labelTitle)
-        view.addSubview(box)
+        view.addSubview(tableView)
         view.addSubview(indicator)
         
         drawDesign()
         
         makeLabelTitle()
-        makeBox()
+        makeTableView()
         makeIndicator()
     }
     
     private func drawDesign() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(RickyMortyCell.self, forCellReuseIdentifier: RickyMortyCell.Identifier.custom.rawValue)
+        
         DispatchQueue.main.async {
-            self.labelTitle.text = "mehmettalhairmak"
+            self.labelTitle.text = "Ricky Morty"
+            self.labelTitle.font = .boldSystemFont(ofSize: 24)
             
-            self.box.backgroundColor = .systemRed
+            self.tableView.rowHeight = self.view.frame.height * 0.15
+            self.tableView.showsVerticalScrollIndicator = false
             
             self.indicator.color = .systemRed
         }
-        indicator.startAnimating()
     }
     
     private func makeLabelTitle() {
@@ -55,8 +98,8 @@ extension ListViewController {
         }
     }
     
-    private func makeBox() {
-        box.snp.makeConstraints { make in
+    private func makeTableView() {
+        tableView.snp.makeConstraints { make in
             make.top.equalTo(labelTitle.snp.bottom).offset(5)
             make.bottom.equalToSuperview()
             make.left.right.equalTo(labelTitle)
